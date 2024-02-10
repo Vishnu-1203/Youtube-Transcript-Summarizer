aa3ba2ce2f3a0378da364176bb36eeb1633c8fe1
Vishnu-1203 <vishnuda333@gmail.com>
2024-02-10 20:39:17 +0530

    minor commit(2nd)

--- youtube-transcript-summarizer/src/App.js
+++ youtube-transcript-summarizer/src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
 import "./App.css";
+import Heading from "./components/heading"; // Update the import to use proper casing
+
 function App() {
-  return <div className="App">hi </div>;
+  return (
+    <div className="App">
+      <Heading>Youtube Transcript Summarizer</Heading>
+    </div>
+  );
 }
 
 export default App;
---
+++ youtube-transcript-summarizer/src/components/heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+const Heading = () => {
+  return <div className="heading">Youtube Transcript summarizer</div>;
+};
+
+export default Heading;
